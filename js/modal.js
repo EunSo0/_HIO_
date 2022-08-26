57c600c54abfa3c593d3f6d1c32e81eaf6982345
EunSo0 <dmstn423@naver.com>
2022-08-26 11:13:08 +0900

    최종커밋

---
+++ js/modal.js
@@ -0,0 +1,51 @@
+const logmodal = document.getElementById("logmodal");
+
+function logmodalOn() {
+  logmodal.style.display = "flex";
+}
+function logmodalOff() {
+  logmodal.style.display = "none";
+}
+
+const joinmodal = document.getElementById("joinmodal");
+
+function joinmodalOn() {
+  joinmodal.style.display = "flex";
+}
+function joinmodalOff() {
+  joinmodal.style.display = "none";
+}
+
+const btnLogin = document.getElementById("btn_login");
+btnLogin.addEventListener("click", (e) => {
+  joinmodalOff();
+  logmodalOn();
+});
+
+const btnJoin = document.getElementById("btn_join");
+btnJoin.addEventListener("click", (e) => {
+  logmodalOff();
+  joinmodalOn();
+});
+
+const btnLog = document.getElementById("btn_log");
+btnLog.addEventListener("click", (e) => {
+  logmodalOn();
+  joinmodalOff();
+});
+
+document.getElementById("log_close_area").addEventListener("click", (e) => {
+  console.log("close");
+  logmodalOff();
+});
+document.getElementById("join_close_area").addEventListener("click", (e) => {
+  joinmodalOff();
+});
+
+modal.addEventListener("click", (e) => {
+  const evTarget = e.target;
+  if (evTarget.classList.contains("#modal_overlay")) {
+    logmodalOff();
+    joinmodalOff();
+  }
+});
